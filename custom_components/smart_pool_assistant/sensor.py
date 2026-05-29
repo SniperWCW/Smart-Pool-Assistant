fc17aebd6006caef44d16230ce20e78fe2d185f9
Steffen <99438572+SniperWCW@users.noreply.github.com>
2026-05-29 12:51:36 +0200

    Update sensor.py

--- custom_components/smart_pool_assistant/sensor.py
+++ custom_components/smart_pool_assistant/sensor.py
@@ -1,1 +1,52 @@
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
+from .coordinator import SmartPoolCoordinator
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    coordinator = SmartPoolCoordinator(hass, entry)
+
+    await coordinator.async_config_entry_first_refresh()
+
+    entities = [
+        PoolChlorSensor(coordinator),
+        PoolPHSensor(coordinator),
+        PoolRecommendationSensor(coordinator),
+    ]
+
+    async_add_entities(entities)
+
+
+class PoolChlorSensor(CoordinatorEntity, SensorEntity):
+    _attr_name = "Pool Chlor"
+    _attr_unique_id = "pool_chlor"
+    _attr_native_unit_of_measurement = "mg/l"
+
+    @property
+    def native_value(self):
+        return self.coordinator.data["chlor"]
+
+
+class PoolPHSensor(CoordinatorEntity, SensorEntity):
+    _attr_name = "Pool pH"
+    _attr_unique_id = "pool_ph"
+
+    @property
+    def native_value(self):
+        return self.coordinator.data["ph"]
+
+
+class PoolRecommendationSensor(CoordinatorEntity, SensorEntity):
+    _attr_name = "Pool Empfehlung"
+    _attr_unique_id = "pool_recommendation"
+
+    @property
+    def native_value(self):
+        chlor = self.coordinator.data["chlor"]
+        grams = self.coordinator.data["chlor_grams"]
+
+        if chlor < 0.5:
+            return f"Stoßchlorung empfohlen: {grams}g"
+
+        return f"Chlor nachdosieren: {grams}g"
