76f12ec7bab40f6d5f9a0ace2843eaf8eac7b94a
Steffen <99438572+SniperWCW@users.noreply.github.com>
2026-05-29 12:51:01 +0200

    Update __init__.py

--- custom_components/smart_pool_assistant/__init__.py
+++ custom_components/smart_pool_assistant/__init__.py
@@ -1,1 +1,18 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+
+PLATFORMS = ["sensor", "number", "switch", "button"]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
